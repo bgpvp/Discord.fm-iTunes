b96f9511eb236c020e5575d35b7243aab224db5b
Hugo <hello@bgpvp.xyz>
2020-04-30 20:36:08 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    lastfm: {
+        key: 'LASTFM_KEY',
+        secret: 'LASTFM_SECRET',
+        username: 'LASTFM_USERNAME'
+    },
+    discord: {
+        // Edit this if you want to use your own client.
+        clientId: '705484377819185253'
+    },
+    icons: {
+        // Edit these if you want to use your own client (logo is largeImage, user is smallImage).
+        logo: 'itunes',
+        user: 'bgpvp'
+    }
+}
